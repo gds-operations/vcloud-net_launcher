b62ccfa95922b2707b18ac861644817a19dec02d
Anna Shipman <anna@annashipman.co.uk>
2014-03-19 09:23:10 +0000

    Remove un-used requires

--- lib/vcloud.rb
+++ lib/vcloud.rb
@@ -11,10 +11,7 @@ require 'vcloud/version'
 require 'vcloud/fog'
 require 'vcloud/core'
 
-require 'vcloud/launch'
 require 'vcloud/net_launch'
-require 'vcloud/vm_orchestrator'
-require 'vcloud/vapp_orchestrator'
 
 module Vcloud
 
