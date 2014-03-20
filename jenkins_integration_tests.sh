d37cc040f7a893208acf7f4faf30b7f9f0a7760c
Anna Shipman <anna@annashipman.co.uk>
2014-03-20 15:30:55 +0000

    Update job for jenkins integration tests
    The integration:all target no longer exists.

--- jenkins_integration_tests.sh
+++ jenkins_integration_tests.sh
@@ -3,9 +3,6 @@ set -e
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 
 ./scripts/generate_fog_conf_file.sh
-
 export FOG_RC=fog_integration_test.config
-
-bundle exec rake integration:all
-
+bundle exec rake integration
 rm fog_integration_test.config
